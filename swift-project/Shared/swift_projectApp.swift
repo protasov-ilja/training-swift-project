a83b8aec06e08d12267d2c17458fcbca6919b5e1
Ilya Protasov <pfltriot@gmail.com>
2021-09-11 09:41:57 +0300

    Added new swift training project

---
+++ swift-project/Shared/swift_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_projectApp.swift
+//  Shared
+//
+//  Created by Ilya Protasov on 11.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct swift_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
