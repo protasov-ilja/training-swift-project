c2f635a6411383bb49e54271169ff21e7e4202f5
Ilya Protasov <pfltriot@gmail.com>
2021-09-11 21:33:54 +0300

    Removed all comments from project's code

--- training-swift-project/ViewController.swift
+++ training-swift-project/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  training-swift-project
-//
-//  Created by Ilya Protasov on 11.09.2021.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
